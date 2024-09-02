023c7415af1e8661d3cf4be6e53d4d2aef411880
khannaanurag1 <khannaanurag1@gmail.com>
2024-09-02 17:38:38 +0530

    Initial Commit

---
+++ src/test/java/org/example/userauthenticationservice_july2024/UserAuthenticationServiceJuly2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userauthenticationservice_july2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationServiceJuly2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
